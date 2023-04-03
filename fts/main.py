0b9d6290e3b90ba92c05c5086dc194218c5903f0
PeaceMind <kexk@outlook.com>
2023-04-03 20:10:40 +0800

    Update main.py

--- fts/main.py
+++ fts/main.py
@@ -4,11 +4,12 @@ from kivy.properties import ObjectProperty
 from kivy.utils import platform
 #from FreeTAKServer.controllers.services import FTS
 import threading
-from jnius import autoclass
 from os import environ
 
-from android.permissions import request_permissions, Permission
-request_permissions(['android.permission.READ_EXTERNAL_STORAGE', 'android.permission.WRITE_EXTERNAL_STORAGE', 'android.permission.POST_NOTIFICATIONS'])
+if platform == 'android':
+    from jnius import autoclass
+    from android import mActivity
+
 
 kv = """
 #:import Thread threading.Thread
@@ -26,42 +27,51 @@ Screen:
         id: switch
         pos_hint: {'center_x': 0.5, 'center_y': 0.3}
         on_press:
-            Thread(target=app.auth).start()
+            Thread(target=app.control_fts).start()
             #app.auth()
-            
+           
 
 """
     
 class Main(MDApp):
     in_class = ObjectProperty(None)
     
-
     def build(self):
+        from android.permissions import request_permissions, Permission
+        request_permissions(['android.permission.READ_EXTERNAL_STORAGE', 'android.permission.WRITE_EXTERNAL_STORAGE', 'android.permission.POST_NOTIFICATIONS'])
+        context =  mActivity.getApplicationContext()
+        service_name = str(context.getPackageName()) + '.ServiceMyfts'
+        self.service = autoclass(service_name)
+        #self.service = None
         return Builder.load_string(kv)
 
-    def start_service(self, name):
-        service_name = 'org.fts.ftsapp.Service' + name
-        service = autoclass(service_name)
+    def start_service(self):        
+        #service_name = 'org.fts.ftsapp.Service' + name
+        #context =  mActivity.getApplicationContext()
+        #service_name = str(context.getPackageName()) + '.Service' + name
+        #self.service = autoclass(service_name)
         #service = autoclass('org.fts.ftsapp.ServiceFts')
-        mActivity = autoclass('org.kivy.android.PythonActivity').mActivity
+        #mActivity = autoclass('org.kivy.android.PythonActivity').mActivity
         #argument = environ.get('PYTHON_SERVICE_ARGUMENT', '')
-        service.start(mActivity,'')   
-        return service
+        self.service.start(mActivity,'')   
+        return self.service
 
-    def auth(self):        
+    def control_fts(self):        
         if self.root.ids.switch.text == 'ON':
             label = self.root.ids.switch
             label.text = "OFF"
             #self.service.stop(self.mActivity)
-            mActivity = autoclass('org.kivy.android.PythonActivity').mActivity
-            self.start_service('Myfts').stop(mActivity)
+            #mActivity = autoclass('org.kivy.android.PythonActivity').mActivity
+            #self.start_service('Myfts').stop(mActivity)
+            self.start_service().stop(mActivity)
         else:
             label = self.root.ids.switch
             label.text = "ON"
             #ex = threading.Event()
             #FTS.start(ex)            
             #self.service.start(self.mActivity, 'small_icon', 'title', 'content' , self.argument)
-            self.start_service('Myfts')
+            #self.start_service('Myfts')
+            self.start_service()
          
      
 Main().run()
